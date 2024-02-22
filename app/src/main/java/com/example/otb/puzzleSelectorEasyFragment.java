4ae297f014e1da3e8731d200e581ba81fa382690
jacobgeorge02 <jacobgeorge296@gmail.com>
2024-02-22 17:18:59 -0500

    Changed navigation for easy puzzle selectors to be accessible

--- app/src/main/java/com/example/otb/puzzleSelectorEasyFragment.java
+++ app/src/main/java/com/example/otb/puzzleSelectorEasyFragment.java
@@ -16,25 +16,13 @@ import com.example.otb.databinding.DifficultyMenuFragmentBinding;
 /**
  * Fragment that shows (Easy, Medium, Hard) difficulty puzzles.
  */
-
 public class puzzleSelectorEasyFragment extends Fragment {
     // View binding object for the DifficultyMenuFragment's layout.
 
 
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.puzzle_selector_easy_fragment, container, false);
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-        View easyButton = view.findViewById(R.id.brightnessPuzzleSelecter);
-
-        easyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // obtain a NavController and navigate
-                Navigation.findNavController(v).navigate(R.id.action_puzzleSelectorEasyFragment_to_brightness_puzzle_easy);
-            }
-        });
-
-        return view;
     }
 }
